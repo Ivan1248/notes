b81cdcff769ffc0bb15ca3f0a9a71f8d97320a9f
Ivan Grubišić <ivan.grubisic.mail@gmail.com>
2026-01-23 13:28:23 +0100

    Update fonts

--- quartz.config.ts
+++ quartz.config.ts
@@ -24,8 +24,8 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
-        body: "Open Sans",
+        header: "Segoe UI",
+        body: "Segoe UI",
         code: "IBM Plex Mono",
       },
       colors: {
